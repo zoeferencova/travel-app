b35c2f27fa235e1027701cd151488ad418efae42
zoeferencova <zoeferencova@gmail.com>
2019-10-08 15:59:23 -0400

    added aria roles to html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const foursquareId = '5S4DIWE5USNCY0HOPLLXFZGYUI1QDCBZVSZ3EQECIZTPWCDA';
 const foursquareKey = '45DSDFCFOGXPP51N4BXWFMB3LHCSMQYYZFBIO1UBT5LMNQTD';
 const foursquareVersion = '20180323'
@@ -6,39 +8,44 @@ const foursquareSearchURL = 'https://api.foursquare.com/v2/venues/'
 const newsKey = '5ea9e10fbd6545338beb1c8f81941f00'
 
 function formatQuery(params) {
-	const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-	return queryItems.join('&')
+	const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+	return queryItems.join('&');
 } 
 
 //generate foursquare information
 function searchFoursquare(query, near) {
-	const endpoint = 'explore'
+	const endpoint = 'explore';
 	const params = {
 		client_id: foursquareId,
 		client_secret: foursquareKey,
 		v: foursquareVersion,
 		near,
 		query,
-		limit: 10
-	}
-	const url = `${foursquareSearchURL}${endpoint}?${formatQuery(params)}`
+		limit: 1
+	};
+	const url = `${foursquareSearchURL}${endpoint}?${formatQuery(params)}`;
 	fetch(url)
 		.then(response =>  {
 			if (response.ok) {
 				return response.json();
 			}
-			throw new Error(response.statusText)
+			throw new Error(response.statusText);
 		})
 		.then(responseJson => getAllDetails(responseJson))
 		.catch(err => {
-			$('.js-error-message').text(`Something went wrong: ${err.message}`).removeClass('hidden')
-		})
+			console.log(err)
+			$('main').addClass('hidden');
+			$('.js-error-message').text(`Sorry, location "${near}" was not found.`).removeClass('hidden');
+		});
 }
 
 function getAllDetails(response) {
-	for (i=0; i < response.response.groups[0].items.length; i++)  {
+	if (response.response.totalResults === 0) {
+		$('.category-error-message').text(`Sorry, there were no results for "${response.response.query}" in ${response.response.headerLocation}.`).removeClass('hidden');
+	}
+	for (let i=0; i < response.response.groups[0].items.length; i++)  {
 		const venueId = response.response.groups[0].items[i].venue.id;
-		getLocationDetails(response, venueId)
+		getLocationDetails(response, venueId);
 	}
 }
 
@@ -47,25 +54,24 @@ function getLocationDetails(response, venueId) {
 		client_id: foursquareId,
 		client_secret: foursquareKey,
 		v: foursquareVersion,
-	}
-	const url = `${foursquareSearchURL}${venueId}?${formatQuery(params)}`
+	};
+	const url = `${foursquareSearchURL}${venueId}?${formatQuery(params)}`;
 	fetch(url)
 		.then(response =>  {
 			if (response.ok) {
 				return response.json();
 			}
-			throw new Error(response.statusText)
+			throw new Error(response.statusText);
 		})
 		.then(responseJson => displayResults(responseJson))
 		.catch(err => {
-			$('.js-error-message').text(`Something went wrong: ${err.message}`).removeClass('hidden')
-		})
-		
+			$('.js-error-message').text(`Something went wrong: ${err.message}`).removeClass('hidden');
+		});		
 }
 
 function returnIfFound(item) {
 	if (item === undefined || !item) {
-		return ''
+		return '';
 	} else {
 		return item;
 	}
@@ -73,9 +79,9 @@ function returnIfFound(item) {
 
 function displayResults(response) {
 	$('.js-search-results').append(
-		`<a href="#map-section" class="link"><li class="places-result-item">
-			<img src=${getPhoto(response)} alt="${response.response.venue.name}" class="places-image">
-			<div>
+		`<button class="places-result-item">
+			<img src=${getFoursquarePhoto(response)} alt="${response.response.venue.name}" class="places-image">
+			<div class="places-text">
 				<div class="places-name-and-rating">
 					<h3 class="name">${response.response.venue.name}</h3>
 					<span class="hidden address">${response.response.venue.location.address}</span>
@@ -88,20 +94,20 @@ function displayResults(response) {
 					<p class="places-description">${returnIfFound(response.response.venue.description)}</p>
 				</div>
 			</div>
-		</li></a>`
-	)
+		</button>`
+	);
 	
 }
 
-function getPhoto(response) {
+function getFoursquarePhoto(response) {
 	if (response.response.venue.bestPhoto === undefined || !response.response.venue.bestPhoto) {
-		return 'images/sample-img.png'
+		return 'images/sample-img.png';
 	} else {
-		const imagePrefix = response.response.venue.bestPhoto.prefix
-		const imageSuffix = response.response.venue.bestPhoto.suffix
-		const imageUrl =`${imagePrefix}200x300${imageSuffix}`
+		const imagePrefix = response.response.venue.bestPhoto.prefix;
+		const imageSuffix = response.response.venue.bestPhoto.suffix;
+		const imageUrl =`${imagePrefix}100x100${imageSuffix}`;
 		return imageUrl;
-	}
+	};
 }
 
 //generate wikipedia information
@@ -121,32 +127,31 @@ function searchWiki(query) {
 	};
 
 	const searchURL = 'https://en.wikipedia.org/w/api.php';
-	const url = `${searchURL}?${formatQuery(params)}`
+	const url = `${searchURL}?${formatQuery(params)}`;
 	fetch(url)
 		.then(response =>  {
 			if (response.ok) {
 				return response.json();
 			}
-			throw new Error(response.statusText)
+			throw new Error(response.statusText);
 		})
-		.then(responseJson => showWikiInfo(responseJson))
+		.then(responseJson => displayWikiInfo(responseJson))
 		.catch(err => {
-			$('.js-error-message').text(`Something went wrong: ${err.message}`).removeClass('hidden')
+			$('.wikipedia-excerpt').text(`No results were found for ${query}`);
 		})
 }
 
-function showWikiInfo(response) {
-	console.log(response)
-	const pageId = Number(response.query.pageids[0])
-	if (pageId === -1) {
+function displayWikiInfo(response) {
+	const pageId = Number(response.query.pageids[0]);
+	if (pageId === -1 || !pageId) {
 		$('.wikipedia-excerpt').text(`Nothing was found for ${query.pages[-1].title}`);
 	} else {
-		const title = response.query.pages[pageId].title
-		const extract = response.query.pages[pageId].extract
-		const url = `https://en.wikipedia.org/wiki/${title}`
+		const title = response.query.pages[pageId].title;
+		const extract = response.query.pages[pageId].extract;
+		const url = `https://en.wikipedia.org/wiki/${title}`;
 		$('.wikipedia-excerpt').html(extract);
-		$('.wikipedia-link').html(`More about ${title} <a href="${url}" target="_blank">here</a>`)
-	}
+		$('.wikipedia-link').html(`More about ${title} <a href="${url}" target="_blank">here</a>`);
+	};
 	
 }
 
@@ -158,10 +163,10 @@ function searchNews(city)  {
 		pageSize: 4,
 		language: 'en',
 		sortBy: 'relevancy'
-	}
+	};
 
-	const searchURL = 'https://newsapi.org/v2/everything'
-	const url = `${searchURL}?${formatQuery(params)}`
+	const searchURL = 'https://newsapi.org/v2/everything';
+	const url = `${searchURL}?${formatQuery(params)}`;
 	fetch(url)
 		.then(response =>  {
 			if (response.ok) {
@@ -169,17 +174,18 @@ function searchNews(city)  {
 			}
 			throw new Error(response.statusText)
 		})
-		.then(responseJson => showNews(responseJson))
+		.then(responseJson => displayNews(responseJson))
 		.catch(err => {
-			$('.js-error-message').text(`Something went wrong: ${err.message}`).removeClass('hidden')
+			console.log(err)
+			$('.js-error-message').text(`Something went wrong: ${err.message}`).removeClass('hidden');
 		})
 }
 
-function showNews(response) {
-	if (response.articles.length === 0) {
-		$('.news-results').append('No news was found.')
+function displayNews(response) {
+	if (!response.articles || response.articles.length === 0) {
+		$('.news-results').append('No news was found.');
 	}
-	for (i=0; i < response.articles.length; i++) {
+	for (let i=0; i < response.articles.length; i++) {
 		const title = response.articles[i].title;
 		const author = response.articles[i].author;
 		const source = response.articles[i].source.name;
@@ -189,9 +195,9 @@ function showNews(response) {
 		const url = response.articles[i].url;
 		$('.news-results').append(
 			`<li class="news-item">
-				<img src="${image}" alt="title" class="news-image">
-				<div>
-					<a href="${url}"><h3 class="name">${title}</h3></a>
+				<img src="${image}" alt="${title}" class="news-image">
+				<div class="news-content">
+					<a href="${url}" target="_blank"><h3 class="name">${title}</h3></a>
 					<div class="news-info">
 						<p class="news-credit">by ${author} from ${source}</p>
 						<p class="news-description">${description}</p>
@@ -199,10 +205,8 @@ function showNews(response) {
 					</div>
 				</div>
 			</li>`
-		)
-	}
-	
-	
+		);
+	};
 }
 
 //create map
@@ -213,37 +217,41 @@ function createMap(query) {
 	  height="100%"
 	  frameborder="0" style="border:0"
 	  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAneTY3_ApOO16hFLSvUESlvJAmiISfJ_c
-	    &q=${query}" allowfullscreen>
-	</iframe>`)
+	    &q=${encodeURIComponent(query)}" allowfullscreen>
+	</iframe>`);
 }
 
 function updateMap() {
-	$('.js-search-results').on('click', '.places-result-item', function() {
+	$('.js-search-results').on('click', '.places-result-item', function(e) {
+		e.preventDefault();
+		$("html, body").animate({ scrollTop: $('#map-section').offset().top }, 500);
 		const name = $(this).find('.name').text();
 		const address = $(this).find('.address').text();
-		console.log(address)
 		$('#map').empty();
 		$('#map').append(`<iframe
 		  width="100%"
 		  height="100%"
 		  frameborder="0" style="border:0"
 		  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAneTY3_ApOO16hFLSvUESlvJAmiISfJ_c
-		    &q=${name} ${address}" allowfullscreen>
-		</iframe>`)
-		$('.map-section span').text(name)
-	})
+		    &q=${encodeURIComponent(name)} ${encodeURIComponent(address)}" allowfullscreen>
+		</iframe>`);
+		$('.map-section span').text(name);
+	});
 }
 
+function toTitleCase(phrase) {
+  return phrase.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+};
+
 function changeHeadings(city, category) {
-	$('.place-word').text(city.charAt(0).toUpperCase() + city.slice(1))
-	$('.map-place-word').text(city.charAt(0).toUpperCase() + city.slice(1))
-	$('.category-word').text(category)
+	$('.place-word').text(city);
+	$('.map-place-word').text(city);
+	$('.category-word').text(category);
 }
 
 function recommendedCategories() {
 	$('.box').on('click', function() {
 		const category = $(this).text();
-		console.log(category)
 		$('#category-search').val(category);
 	})
 }
@@ -261,22 +269,28 @@ function changeLayout() {
 	$('#category-search').val('').addClass('small-search');
 }
 
+function clearPrevious() {
+	$('.js-search-results').empty();
+	$('.news-results').empty();
+	$('.wikipedia-info p').empty();
+	$('.js-error-message').empty();
+	$('.category-error-message').empty();
+}
+
 function watchForm() {
 	$('form').submit(event => {
-		$('.js-search-results').empty();
-		$('.news-results').empty();
-		$('.wikipedia-info p').empty();
+		clearPrevious();
 		event.preventDefault();
-		const city = $('#city-search').val();
+		const city = toTitleCase($('#city-search').val());
 		const category = $('#category-search').val();
 		searchFoursquare(category, city);
 		searchWiki(city);
 		searchNews(city);
 		createMap(city);
-		changeHeadings(city, category)
+		changeHeadings(city, category);
 		updateMap();
 		changeLayout();
-	})
+	});
 }
 
 $(watchForm())
